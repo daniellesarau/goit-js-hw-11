314ae58196951cea0eea373db7f86f264a0d866a
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2023-07-14 09:09:44 +0300

    api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import { Notify } from 'notiflix';
-import ImagesApiService from './api';
-import { Axios } from 'axios';
+// import ImagesApiService from './api';
+import axios, * as others from 'axios';
+
+const API_KEY = '38190446-4bb1c0206a67f58024ebd8e6f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const searchParams = new URLSearchParams({
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 40,
+});
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getImage() {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${searchParams}&page=${this.page}`
+    );
+    this.page += 1;
+
+    return response.data;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
 
 const imageApiService = new ImagesApiService();
 const form = document.getElementById('search-form');
